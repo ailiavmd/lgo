8f1db5ab0e9cefdd89b5ee677ca841752411c5e0
amartin <amartin@esv.com.ve>
2019-02-18 15:15:02 -0400

    modificaciones

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,16 @@
-$('.search .input-group.date').datepicker({
-    autoclose: true,
-    format: 'dd-mm-yyyy'
+$(document).ready(function(){
+
+    $('.search .input-group.date').datepicker({
+        autoclose: true,
+        format: 'dd-mm-yyyy'
+    });
+
+    $('.navigation a, .cta .button').on('click', function(e){
+        e.preventDefault();
+        var hash = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top - 60
+        }, 400);
+    })
+
 });
